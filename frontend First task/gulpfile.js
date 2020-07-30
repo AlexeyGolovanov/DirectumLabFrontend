03d72c2575dd42faa1f626b6a0de78e1db9e2aa5
Golovanov A.V <Golovanov_AV@directum.ru>
2020-07-30 10:48:53 +0400

    new repository

---
+++ frontend First task/gulpfile.js
@@ -0,0 +1,66 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const concat = require('gulp-concat');
+const del = require('del');
+const imageMin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+const pathStyles = [
+	'src/sass/reset.scss',
+	'src/sass/header.scss',
+	'src/sass/index.scss',
+	'src/sass/cards.scss',
+	'src/sass/sidebar.scss',
+	'src/sass/invite.scss',
+	'src/sass/roundHistory.scss',
+	'src/sass/main.scss',
+	'src/sass/media.scss'
+]
+
+const styles = () => {
+	return gulp.src(pathStyles)
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(concat('style.css')) 
+		.pipe(cleanCSS({
+			level: 2
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.stream());
+}
+
+const pugs = () => {
+	return gulp.src("src/pug/*.pug")
+		.pipe(pug())
+		.pipe(gulp.dest("./dist"))
+		.pipe(browserSync.stream());
+}
+
+const images = () => {
+	return gulp.src("src/img/**/*.png")
+		.pipe(imageMin())
+		.pipe(gulp.dest("./dist/img"));
+}
+
+const watch = () => {
+	browserSync.init({
+		server: {
+			baseDir: "./dist"
+		}
+	});
+	gulp.watch('src/pug/**/*.pug', pugs);
+	gulp.watch('src/sass/**/*.sass', styles);
+	gulp.watch('src/sass/**/*.scss', styles);
+	gulp.watch('src/*html').on('change', browserSync.reload);
+}
+
+const clean = () => {
+	return del(['dist/*']);
+}
+
+gulp.task('build', gulp.series(clean, gulp.parallel(pugs, styles, images)));
+gulp.task('watch', gulp.series('build', watch));
